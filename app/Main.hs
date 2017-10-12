d9846befe37f381f8dd3084a4e81d96414cec016
Craig  Nolan <craignolan@Craigs-MacBook-Pro.local>
2017-10-12 16:37:02 +0100

    Hello server

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Lib
+import Network.Socket
+
 main :: IO ()
-main = someFunc
+main = do
+    sock <- socket AF_INET Stream 0 -- create socket Family SocketType Protocol Number
+    setSocketOption sock ReuseAddr 1 -- make reuseable, setSocketOption Socket SocketOption Int
+    bind sock (SockAddrInet 4242 iNADDR_ANY) -- listen on TCP port 4242, bind Socket SockAddr
+    listen sock 2 -- max 2 connections, listen Socket Int
+    mainLoop sock -- todo
+
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+    connection <- accept sock -- accept a connection and handle it
+    runConn connection
+    mainLoop sock
+
+runConn :: (Socket, SockAddr) -> IO ()
+runConn (sock, _) = do
+    send sock "Hi There!\n"
+    print "Hi\n"
+    close sock
