9af1b7da62343e1f25b44de9acec0c7a506c8758
Craig Nolan <nolancr@tcd.ie>
2017-11-23 16:54:59 +0000

    Chat msg for disonnection (test)

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -67,9 +67,9 @@ removeClient roomRef client rooms = do
         Just c -> do
             let roomName = (roomStr c)
             clientMap <- atomically $ do readTVar (clients c)
+            sendMessage (Chat (show roomRef) name $ name ++ " has left this chatroom.") roomRef rooms client
             hPutStr (clientHandle client) $ "LEFT_CHATROOM: " ++ (show roomRef) ++ "\nJOIN_ID: " ++ (show $ clientId client) ++ "\n"
             let name = clientName client
-            sendMessage (Chat (show roomRef) name $ name ++ " has left this chatroom.") roomRef rooms client
             let newMap = Map.delete (clientId client) clientMap
             atomically $ do writeTVar (clients c) newMap
             print "client left"
